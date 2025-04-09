994031f7fc2cceee285eccfee8df14323fe519e2
Aluwani24 <aluwani024@gmail.com>
2025-04-09 21:11:03 +0200

    fixed spelling error

--- variables.js
+++ variables.js
@@ -28,9 +28,9 @@ let num2 = 5
 let sum = num1 + num2
 let difference = num1-num2
 let product = num1 * num2
-let quoteient = num1 / num2
+let quotient = num1 / num2
 
 console.log("Sum:",sum)
 console.log("Difference:",difference)
 console.log("Product:",product)
-console.log("Quotient:",quoteient)
+console.log("Quotient:",quotient)
