24aa2a3f2bc25be4049746c3f019e54dcd85935c
Aluwani24 <aluwani024@gmail.com>
2025-04-09 20:46:07 +0200

    declared a variable and re-assigned it with a new value

--- variables.js
+++ variables.js
@@ -9,3 +9,11 @@ let isStudent = true
 console.log("My name:",fname);
 console.log("My age:",age);
 console.log("Student:",isStudent);
+
+//exercise 2
+//declare a variable and assign a value to it, re-assign a value to it again
+
+let favoriteColer = "Black"
+//re-assigning a value
+favoriteColer = "Blue"
+console.log("Favorite color:", favoriteColer)
