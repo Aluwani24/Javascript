a86bf874977d08cc700d63357501347b03460f41
Aluwani24 <aluwani024@gmail.com>
2025-04-09 20:33:26 +0200

    declared variables and printed the results

---
+++ variables.js
@@ -0,0 +1,11 @@
+//exercise 1
+//declaring variables
+//print all three
+
+let fname = "Aluwani"
+let age = 18
+let isStudent = true
+
+console.log("My name:",fname);
+console.log("My age:",age);
+console.log("Student:",isStudent);
