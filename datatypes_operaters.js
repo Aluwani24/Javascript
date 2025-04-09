9535c068dbacb97198efdfbec288d1fa72cd810f
Aluwani24 <aluwani024@gmail.com>
2025-04-09 21:47:49 +0200

    declares integer and floating numbers

---
+++ datatypes_operaters.js
@@ -0,0 +1,16 @@
+//exercise 1
+// 
+let intNum = 10
+let floatNum = 5.5
+
+let sum = intNum + floatNum
+let sub = intNum - floatNum
+let mult = intNum * floatNum
+let mod = intNum % 4
+let exp = intNum ** 3
+
+console.log("Addition:",sum);
+console.log("Subtraction:",sub);
+console.log("Multiplecation:",mult);
+console.log("Modulus:",mod);
+console.log("Exponential:",exp)
